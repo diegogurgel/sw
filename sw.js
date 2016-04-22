9258cf4df18e41ca54a50d9e9ffee50cb17c2ba5
diego <diego@diegogurgel.com>
2016-04-22 00:25:21 -0300

    add root directory to list

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
 var CACHE_NAME = 'sw-v1';
 
 var urls = [
-	'index.html',
+	'./',
 	'scripts/main.js',
 	'styles/main.css'
 ]
 self.addEventListener('install',event=>{
-	event.waitUntil(caches.open(CACHE_NAME).then(cache=>{
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache=>{
 			return cache.addAll(urls);
 		})
 	);
@@ -14,9 +15,6 @@ self.addEventListener('install',event=>{
 
 self.addEventListener('fetch',event=>{
 	event.respondWith(caches.match(event.request).then(response=>{
-		if(response){
-			return response;
-		}
-		return fetch(event.request);
+		return response || fetch(event.request);
 	}));
 });
